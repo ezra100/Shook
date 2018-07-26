b1651c66b7a11d7be776f12dfbb38feb024fdc82
ezra250@gmail.com <ezra250@gmail.com>
2018-07-27 00:30:33 +0300

    fixed dotdotdot

--- angular-app/src/app/product/product.component.ts
+++ angular-app/src/app/product/product.component.ts
@@ -12,8 +12,8 @@ import {categoryNames, Product} from '../../../../types';
 export class ProductComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     let subtitle = $(this.element.nativeElement).find('.product-subtitle');
-    let height = subtitle.outerHeight();
-    subtitle.dotdotdot && subtitle.dotdotdot({height});
+    //let height = subtitle.outerHeight();
+    subtitle.dotdotdot && setTimeout(() => subtitle.dotdotdot({watch: false /*as a reminder - watching means polling every 100 or 500 ms, very expensive  performance wise*/}), 500);
   }
   @Input() product: Product;
   categoryNames = categoryNames;
